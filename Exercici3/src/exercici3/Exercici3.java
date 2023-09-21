a0b210ee58bd3ff1e595f16629c483d02a14ca09
XanaDevelops <danielgv.space@gmail.com>
2023-09-21 17:30:38 +0200

    añadido portatil

--- Exercici3/src/exercici3/Exercici3.java
+++ Exercici3/src/exercici3/Exercici3.java
@@ -3,7 +3,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
 package exercici3;
-
+import exercici3.Figures.*;
 /**
  *
  * @author danie
---
+++ Exercici3/src/exercici3/Figures/Figura.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package exercici3.Figures;
+
+/**
+ *
+ * @author daniel
+ */
+public abstract class Figura {
+    protected double area;
+    protected double perimetro;
+    
+    public abstract void cArea();
+    public abstract void cPerimetro();
+}
