873301c31d382082c2435c51a8ff3015d397eaac
Daniel GV <danielgv.space@gmail.com>
2023-09-25 23:31:52 +0200

    Añadido comentario Enunciado

--- Exercici3/src/exercici3/Exercici3.java
+++ Exercici3/src/exercici3/Exercici3.java
@@ -12,7 +12,19 @@ import java.util.Random;
  * @author danie
  */
 public class Exercici3 {
+    /*
+    Volem representar diferents 4 tipus de Figura (Cercle,Triangle, Rectangle i Quadrat). Totes les Figures tenen una àrea i un perímetre.
 
+        Heu de generar de manera aleatòria 10000 figures i emmagatzemar-les en una Collection
+        Heu de calcular la suma de les àrees i perímetres de totes les figures
+        Heu de calcular la suma de les àrees i perímetres de cada tipus de figura
+        Heu de calcular l’àrea màxima i mínima de totes les figures i per a cada tipus de figures.
+        Heu de poder ordenar les figures per àrees i per perímetre i mostrar les 10 primeres.
+        Heu de documentar el codi amb JavaDoc
+
+    Nota: Els resultats s'han de mostrar per la sortida estàndard i heu d'utilitzar el mínim número  de while/for./*
+    
+    */
     /**
      * @param args the command line arguments
      */
