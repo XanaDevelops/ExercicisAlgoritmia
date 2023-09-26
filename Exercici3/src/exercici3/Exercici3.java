b550e87acc3da065c8502080fe33a97f213d8570
Pere55 <truficonosmanut@gmail.com>
2023-09-26 16:17:10 +0200

    He implementado los comparetors y añadido unos metodos que buscan maximos i minimos selectivos

--- Exercici3/src/exercici3/Exercici3.java
+++ Exercici3/src/exercici3/Exercici3.java
@@ -6,12 +6,14 @@ package exercici3;
 
 import exercici3.Figures.*;
 import java.util.Random;
+import java.util.Arrays;
 
 /**
  *
  * @author danie
  */
 public class Exercici3 {
+
     /*
     Volem representar diferents 4 tipus de Figura (Cercle,Triangle, Rectangle i Quadrat). Totes les Figures tenen una àrea i un perímetre.
 
@@ -24,39 +26,85 @@ public class Exercici3 {
 
     Nota: Els resultats s'han de mostrar per la sortida estàndard i heu d'utilitzar el mínim número  de while/for./*
     #esto es una prueba jajajajajajajajajajjajajajajajajajajajajajajajajajajajajajajajajajja
-    */
+     */
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        Exercici3 ex = new Exercici3();
-        Figura[] temp = ex.creadorDeFigures(3);
-        for (int i = 0; i < 3; i++) {
-            System.out.print(temp[i].getClass().getSimpleName() + ", ");
-
-        }
+       
 
     }
 
-    public double[] sumaPerimetroIArea(int nFigures,Figura[] temp) {
-        double[] resultat = new double[2]; 
-       for (int i = 0; i < nFigures; i++) {
-            resultat[0]+=temp[i].getPerimetro();
-            resultat[1]+=temp[i].getArea();
+    public Figura[] cercadorSelectiu(Figura[] figures, String tipus) {
+        int x = 0;
+        Figura[] temp = new Figura[4];
+        while (!(figures[x].getClass().equals(tipus))) {
+            temp[0] = figures[x];
+            temp[1] = figures[x];
+            temp[2] = figures[x];
+            temp[3] = figures[x];
+            x++;
+        }
+        for (int i = 1; i < figures.length; i++) {
+            if (temp[0].getArea() < figures[i].getArea() && (figures[i].getClass().getSimpleName().equals(tipus))) {
+                temp[0] = figures[i];
+            } else if (temp[0].getArea() > figures[i].getArea() && (figures[i].getClass().getSimpleName().equals(tipus))) {
+                temp[2] = figures[i];
+            }
+            if (temp[1].getPerimetro() < figures[i].getPerimetro() && (figures[i].getClass().getSimpleName().equals(tipus))) {
+                temp[1] = figures[i];
+            } else if (temp[0].getPerimetro() > figures[i].getPerimetro() && (figures[i].getClass().getSimpleName().equals(tipus))) {
+                temp[3] = figures[i];
+            }
 
         }
-     
+        return temp;
+    }
+
+    public Figura[] cercador(Figura[] figures) {
+        Figura temp[] = new Figura[4];
+        temp[0] = figures[0];
+        temp[1] = figures[0];
+        temp[2] = figures[0];
+        temp[3] = figures[0];
+        for (int i = 1; i < figures.length; i++) {
+            if (temp[0].getArea() < figures[i].getArea()) {
+                temp[0] = figures[i];
+            } else {
+                temp[2] = figures[i];
+            }
+            if (temp[1].getPerimetro() < figures[i].getPerimetro()) {
+                temp[1] = figures[i];
+
+            } else {
+                temp[3] = figures[i];
+            }
+
+        }
+        return temp;
+    }
+
+    public double[] sumaPerimetroIArea(int nFigures, Figura[] temp) {
+        double[] resultat = new double[2];
+        for (int i = 0; i < nFigures; i++) {
+            resultat[0] += temp[i].getPerimetro();
+            resultat[1] += temp[i].getArea();
+
+        }
+
         return resultat;
 
     }
-    public double[] sumaSelectiva(int nFigures,Figura[] temp,String selecion){
-       double[] resultat = new double[2]; 
-       for (int i = 0; i < nFigures; i++) {
-           if(selecion.equals(temp.getClass().getSimpleName())){
-            resultat[0]+=temp[i].getPerimetro();
-            resultat[1]+=temp[i].getArea();}
 
-        }  
+    public double[] sumaSelectiva(int nFigures, Figura[] temp, String selecion) {
+        double[] resultat = new double[2];
+        for (int i = 0; i < nFigures; i++) {
+            if (selecion.equals(temp.getClass().getSimpleName())) {
+                resultat[0] += temp[i].getPerimetro();
+                resultat[1] += temp[i].getArea();
+            }
+
+        }
 
         return resultat;
     }
