909dbf74489e5fd6de808d281d7122775e5955dc
Daniel GV <danielgv.space@gmail.com>
2023-10-22 18:28:13 +0200

    Ex 6

---
+++ Exercici5/dist/javadoc/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"exercici5"},{"l":"persones"}];updateSearchResults();
---
+++ Exercici5/dist/javadoc/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"persones","l":"Professor.Areas"},{"p":"persones","l":"Estudiant"},{"p":"exercici5","l":"Exercici5"},{"p":"persones","l":"Persona"},{"p":"persones","l":"Professor"},{"p":"persones","l":"Estudiant.Programes"}];updateSearchResults();
