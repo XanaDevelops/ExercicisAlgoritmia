63ea5a4e9f354d64b4f1e06dc47bb8842869fc28
Pere55 <truficonosmanut@gmail.com>
2023-10-25 22:41:20 +0200

    Creacion de algunos tests i del javaDoc

--- Exercici6/src/exercici6/Exercici6.java
+++ Exercici6/src/exercici6/Exercici6.java
@@ -16,13 +16,27 @@ public class Exercici6 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+      
+       Character elem = 'E';
+       Character[] elems = {'B','A','G','F','E'};
+       Arrays.sort(elems);
+       
+       System.out.println("El caracter "+elem+" se encuentra en la posicion "+ binarySearch(elem, elems));
         
     }
     
-    public static <E extends Comparable<E>> int binarySearch(E elem, E[] elems){
+    /**
+     *Este metodo tiene como finalidad llevar a cabo la busqueda de un elemento dentro de un array usando el metodo binario.
+     *La precondicion para poder llevar a cabo la busqueda binaria sera que los elementos que se encuentran dentro del array han de estar ordenados de mayor a menor.
+     * La condicion de salida es que el metodo ha de sacar un entero que este entre el -1 i la longitud menos uno del array siendo menos uno el valor que saque cuano el elemento no exista dentro del array i un numero entre el 0 i la longitud maxima del array la posicion del elemento cuando exista
+     * @param <E> elemento generico 
+     * @param elem elemento generico que se busca 
+     * @param elems array de elementos ordenado 
+     * @return int que indica la posicion del elemento , si este es -1 significa que el elemento que se busca esta fuera del array
+     */
+    public  static<E extends Comparable<E>> int binarySearch(E elem, E[] elems){
         if(elem==null || elems==null){
-            throw new NullPointerException();
+          throw new NullPointerException();   
         }
         if(elems.length==0){
             return -1;
@@ -44,7 +58,17 @@ public class Exercici6 {
         return -1;
     }
     
-    public static <E> int binarySearch(E elem, E[] elems, Comparator<E> c){
+    /**
+      *Este metodo tiene como finalidad llevar a cabo la busqueda de un elemento dentro de un array usando el metodo binario.
+     *La precondicion para poder llevar a cabo la busqueda binaria sera que los elementos que se encuentran dentro del array han de estar ordenados de mayor a menor.
+     * La condicion de salida es que el metodo ha de sacar un entero que este entre el -1 i la longitud menos uno del array siendo menos uno el valor que saque cuano el elemento no exista dentro del array i un numero entre el 0 i la longitud maxima del array la posicion del elemento cuando exista
+     * @param <E> elemento generico 
+     * @param elem elemento a buscar
+     * @param elems array de elementos ordenado
+     * @param c comparator que se usa para comparar los elmentos del array con el elemento a buscar
+     * @return int que indica la posicion del elemento , si este es -1 significa que el elemento que se busca esta fuera del array
+     */
+    public static<E> int binarySearch (E elem, E[] elems, Comparator<E> c){
         if(elem==null || elems==null || c==null){
             throw new NullPointerException();
         }
--- Exercici6/test/exercici6/Exercici6Test.java
+++ Exercici6/test/exercici6/Exercici6Test.java
@@ -11,32 +11,20 @@ import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 import static org.junit.Assert.*;
-
+import java.lang.NullPointerException;
 /**
  *
  * @author danie
  */
 public class Exercici6Test {
     
+    /**
+     *
+     */
     public Exercici6Test() {
     }
     
-    @BeforeClass
-    public static void setUpClass() {
-    }
-    
-    @AfterClass
-    public static void tearDownClass() {
-    }
-    
-    @Before
-    public void setUp() {
-    }
-    
-    @After
-    public void tearDown() {
-    }
-
+   
     /**
      * Test of main method, of class Exercici6.
      */
@@ -50,12 +38,27 @@ public class Exercici6Test {
     }
 
     /**
-     * Test of binarySearch method, of class Exercici6.
+     * Este test comprueba que la busqueda de un elemento que se encuentra  en la parte superior del  array se lleve acabo de forma correcte dandonos la posicion correcta del elemento dentro del array
      */
     @Test
-    public void testBinarySearch_GenericType_GenericType() {
-        System.out.println("binarySearch");
+    public void testLimiteSuperior() {
+        System.out.println("LimitSuperior");
         Integer elem = 3;
+        Integer[] elems = {1,2,3};
+        int expResult = Arrays.binarySearch(elems, elem);
+        int result = Exercici6.binarySearch(elem, elems);
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        //fail("The test case is a prototype.");
+    }
+    
+    /**
+     *Este test comprueba que la busqueda de un elemento que se encuentra  en la parte inferior del  array se lleve acabo de forma correcte dandonos la posicion correcta del elemento dentro del array
+     */
+    @Test
+    public void testLimiteInferior() {
+        System.out.println("LimitInferior");
+        Integer elem = 1;
         Integer[] elems = {1,2,3};
         int expResult = Arrays.binarySearch(elems, elem);
         int result = Exercici6.binarySearch(elem, elems);
@@ -65,17 +68,43 @@ public class Exercici6Test {
     }
 
     /**
-     * Test of binarySearch method, of class Exercici6.
+     *Este test comprueba a ver que passa cuando intentas buscar un elemento que no existe dentro del array i es mas pequeño que el primer elemento que si esxiste dentro del array
      */
     @Test
-    public void testBinarySearch_3args() {
-        fail("TODO");
-        System.out.println("binarySearch");
-        int expResult = 0;
-        int result = Exercici6.binarySearch(null, null);
+    public void testForaDeLImitInferior() {
+        System.out.println("ForaDeLimitInferior");
+        Integer elem = 0;
+        Integer[] elems = {1,2,3};
+        int expResult = Arrays.binarySearch(elems, elem);
+        int result = Exercici6.binarySearch(elem, elems);
         assertEquals(expResult, result);
         // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+        //fail("The test case is a prototype.");
     }
+
+    /**
+     *Este test comprueba a ver que passa cuando intentas buscar un elemento que no existe dentro del array i es mas grande que el ultimo elemento que si esxiste dentro del array
+     */
+    @Test
+    public void testForaDeLImitSuperior() {
+        System.out.println("ForaDeLimitSuperior");
+        Integer elem = 0;
+        Integer[] elems = {1,2,3};
+        int expResult = Arrays.binarySearch(elems, elem);
+        int result = Exercici6.binarySearch(elem, elems);
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        //fail("The test case is a prototype.");
+    }   
+    /**
+     * Este test mira si se lanza la exception correcta cuando el array o el elemento introducido son nulos
+     */
     
+    @Test
+    public void testBinarySearchWithNullArguments() {
+        System.out.println("ValorNull");
+        assertThrows(NullPointerException.class, () -> {
+            Exercici6.binarySearch(null, null); 
+        });
+    }
 }
