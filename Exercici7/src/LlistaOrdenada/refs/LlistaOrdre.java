472d2937fe7a9047db4bebfe583db4c9deedcf49
Daniel GV <danielgv.space@gmail.com>
2023-10-30 23:11:42 +0100

    test, javadoc, O(x) de LlistaOrdre (refs)

--- Exercici7/src/LlistaOrdenada/refs/LlistaOrdre.java
+++ Exercici7/src/LlistaOrdenada/refs/LlistaOrdre.java
@@ -13,53 +13,58 @@ import LlistaOrdenada.Node;
  * @param <E>
  */
 public class LlistaOrdre<E extends Comparable<E>> implements LlistaOrdenada<E> {
+
     private Node<E> first;
-    
+
     /**
+     * Constructor d'una Llista Ordenada
      *
+     * @see LlistaOrdenada
      */
-    public LlistaOrdre(){
+    public LlistaOrdre() {
         first = null;
     }
-    
+
     /**
+     * Afegeix un Element a la llista. Després s'ordena per l'ordre natural Pre:
+     * Element no null Post: Llista ordenada amb l'element O(n)
      *
      * @param e
      */
     @Override
     public void add(E e) {
         Node<E> node = new Node<>(e);
-        if(first==null){
-            first=node;
+        if (first == null) {
+            first = node;
             node.setNext(null);
             node.setPrev(null);
             return;
         }
         Node<E> comp = first;
-        while(comp.getNext()!=null && node.compareTo(comp)>=0){
+        while (comp.getNext() != null && node.compareTo(comp) >= 0) {
             comp = comp.getNext();
         }
-        
-        if(node.compareTo(comp)>=0){
+
+        if (node.compareTo(comp) >= 0) {
             comp.setNext(node);
             node.setPrev(comp);
             node.setNext(null);
-        }else{
-            if(comp.getPrev()!=null){
+        } else {
+            if (comp.getPrev() != null) {
                 node.setPrev(comp.getPrev());
                 comp.getPrev().setNext(node);
-                
-            }else{
-                first=node;
+
+            } else {
+                first = node;
             }
             node.setNext(comp);
             comp.setPrev(node);
         }
-        
+
     }
 
     /**
-     *
+     * Pre: Res Post: Llista buida O(1)
      */
     @Override
     public void clear() {
@@ -67,47 +72,75 @@ public class LlistaOrdre<E extends Comparable<E>> implements LlistaOrdenada<E> {
     }
 
     /**
+     * Pre: element no nul a cercar Post: true si el citat element existeix,
+     * false en cas contrari O(n)
      *
-     * @param e
-     * @return
+     * @param e Element a cercar
+     * @return Si existeix l'element
      */
     @Override
     public boolean contains(E e) {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+        Node<E> node = first;
+        while (node != null) {
+            if (node.getElem().compareTo(e) == 0) {
+                return true;
+            }
+            node = node.getNext();
+        }
+        return false;
     }
 
     /**
+     * Pre: Cap Post: true si existeix al manco un element, sino false O(1)
      *
      * @return
      */
     @Override
     public boolean isEmpty() {
-        return first==null;
+        return first == null;
     }
 
     /**
+     * Pre: Element no null a eliminar Post: Llista ordenada amb l'element
+     * eliminat O(n)
      *
-     * @param e
+     * @param e Element
      */
     @Override
     public void remove(E e) {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+        Node<E> node = first;
+        while (node != null) {
+            if (node.getElem().compareTo(e) == 0) {
+                if (node.getPrev() != null) {
+                    node.getPrev().setNext(node.getNext());
+                }else{
+                    first=node.getNext(); //es el primer
+                }
+                if (node.getNext() != null) {
+                    node.getNext().setPrev(node.getPrev());
+                }
+                return;
+            }
+            node = node.getNext();
+        }
+
     }
-    
+
     /**
+     * Retorna {@link String} dels elements de la Llista
      *
-     * @return
+     * @return String
      */
     @Override
-    public String toString(){
+    public String toString() {
         String s = "";
         Node<E> n = first;
-        while(n!=null){
-            s+=n.getElem()+"\n";
-            n=n.getNext();
+        while (n != null) {
+            s += n.getElem() + ",";
+            n = n.getNext();
         }
         return s;
-        
+
     }
-    
+
 }
---
+++ Exercici7/test/LlistaOrdenada/refs/LlistaOrdreTest.java
@@ -0,0 +1,142 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/UnitTests/JUnit4TestClass.java to edit this template
+ */
+package LlistaOrdenada.refs;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author danie
+ */
+public class LlistaOrdreTest {
+    
+    public LlistaOrdreTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    /**
+     * Test of add method, of class LlistaOrdre.
+     */
+    @Test
+    public void testAdd() {
+        System.out.println("add");
+        Integer a = 3;
+        LlistaOrdre<Integer> instance = new LlistaOrdre<>();
+        instance.add(a);
+        assertTrue(!instance.isEmpty());
+        assertTrue(instance.contains(a));
+    }
+    
+    /**
+     * Test of add method, of class LlistaOrdre.
+     */
+    @Test
+    public void testAdd2() {
+        System.out.println("add2");
+        Integer[] a = {2,4,6,8};
+        LlistaOrdre<Integer> instance = new LlistaOrdre<>();
+        for(int x:a){
+            instance.add(x);
+        }
+        assertEquals("2,4,6,8,",instance.toString());
+        assertTrue(!instance.isEmpty());
+        instance.add(3);
+        instance.add(1);
+        instance.add(9);
+        assertEquals("1,2,3,4,6,8,9,", instance.toString());
+    }
+
+    /**
+     * Test of clear method, of class LlistaOrdre.
+     */
+    @Test
+    public void testClear() {
+        System.out.println("clear");
+        LlistaOrdre instance = new LlistaOrdre();
+        instance.add(3);
+        instance.add(1);
+        instance.add(9);
+        assertTrue(!instance.isEmpty());
+        instance.clear();
+        assertTrue(instance.isEmpty());
+
+    }
+
+    /**
+     * Test of contains method, of class LlistaOrdre.
+     */
+    @Test
+    public void testContains() {
+        System.out.println("contains");
+        LlistaOrdre instance = new LlistaOrdre();
+        instance.add(3);
+        instance.add(1);
+        instance.add(9);
+        boolean result = instance.contains(3);
+        assertEquals(true, result);
+        assertTrue(!instance.contains(0));
+    }
+
+    /**
+     * Test of isEmpty method, of class LlistaOrdre.
+     */
+    @Test
+    public void testIsEmpty() {
+        System.out.println("isEmpty");
+        LlistaOrdre instance = new LlistaOrdre();
+        boolean expResult = true;
+        boolean result = instance.isEmpty();
+        assertEquals(expResult, result);
+
+    }
+
+    /**
+     * Test of remove method, of class LlistaOrdre.
+     */
+    @Test
+    public void testRemove() {
+        System.out.println("remove");
+        LlistaOrdre instance = new LlistaOrdre();
+        instance.add(3);
+        instance.add(1);
+        instance.add(9);
+        instance.remove(3);
+        assertEquals("1,9,",instance.toString());
+        instance.remove(2);
+        assertEquals("1,9,",instance.toString());
+        instance.remove(1);
+        assertEquals("9,", instance.toString());
+        instance.remove(9);
+        assertTrue(instance.isEmpty());
+        instance.add(3);
+        instance.add(1);
+        instance.add(9);
+        instance.remove(9);
+        assertEquals("1,3,",instance.toString());
+    }
+
+   
+    
+}
