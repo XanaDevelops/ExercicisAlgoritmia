f082cf534d63fe09ddd56f6dd1ddd3e1b7777721
XanaDevelops <danielgv.space@gmail.com>
2023-11-09 10:03:30 +0100

    esqueleto EX9

---
+++ Exercici9/src/peak/PeakImpl.java
@@ -0,0 +1,22 @@
+package peak;
+
+import java.awt.Point;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+/**
+ *
+ * @author antoni
+ */
+public class PeakImpl implements Peak {
+
+    @Override
+    public Point peak(int[] a) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    
+}
