caaa993bbcffa47364a72394fba2515d3d3a7629
Daniel GV <danielgv.space@gmail.com>
2023-11-23 22:51:28 +0100

    Analisi de casos

--- Exercici10/src/hanoi/Hanoi.java
+++ Exercici10/src/hanoi/Hanoi.java
@@ -16,7 +16,12 @@ public interface Hanoi<E extends Comparable<E>>{
      * Mètode recursiu que resol Hanoi
      *@pre ∀i (A.lenght> i=>0)∧ (A[i+1]>A[i])∧(B.lenght=0∧C.lenght=0)
      * @post ∀i (C.lenght> i=>0)∧ (C[i+1]>C[i])∧(A.lenght=0∧B.lenght=0)
-     * @ord  O(2**n )
+     * @ord  O(2**n)
+     * El cas base consisteix en un unic element en la pila A, i les altres dues estan buides
+     * Per resoldre el cas base el mou l'element de A a C i es retorna.
+     * Un cas general te una altura h a la pila A i les altres buides.
+     * Mitjançant recursivitat es va reduint el tamany de la torre mentres es van fent intercanvis
+     * dels elements de les torres A i C i s'alterna l'ordre de les torres B i C; i A i B respectivament.
      * @param h Altura de les torres
      * @param a torre a pila inicila de hanoi
      * @param b torre b  pila intermitja de les torres  de Hanoi
