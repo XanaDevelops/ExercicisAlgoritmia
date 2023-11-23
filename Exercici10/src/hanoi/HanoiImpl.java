b92509ecefea50ef71510f72a8309b8113412da2
Pere55 <truficonosmanut@gmail.com>
2023-11-23 09:45:41 +0100

    canvios basicos

--- Exercici10/src/hanoi/HanoiImpl.java
+++ Exercici10/src/hanoi/HanoiImpl.java
@@ -1,26 +1,28 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package hanoi;
 
 import java.util.Stack;
 
-/**
- *
- * @author antoni
- */
 public class HanoiImpl<E extends Comparable<E>> implements Hanoi<E> {
 
     @Override
     public void recursiuHanoi(int h, Stack<E> a, Stack<E> b, Stack<E> c) {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+        if (h == 1) {
+            moverDisco(a, c);
+        } else {
+            recursiuHanoi(h - 1, a, c, b);
+            moverDisco(a, c);
+            recursiuHanoi(h - 1, b, a, c);
+        }
     }
 
     @Override
     public void iteratiuHanoi(int h, Stack<E> a, Stack<E> b, Stack<E> c) {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+        
     }
 
+    private void moverDisco(Stack<E> origen, Stack<E> destino) {
+        E disco = origen.pop();
+        destino.push(disco);
+        System.out.println("Mover disco " + disco + " desde " + origen + " hasta " + destino);
+    }
 }
