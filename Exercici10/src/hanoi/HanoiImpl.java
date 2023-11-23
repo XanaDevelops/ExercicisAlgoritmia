ea917810f7ca9d0fe367286cc455e98522e01363
Daniel GV <danielgv.space@gmail.com>
2023-11-23 18:42:17 +0100

    Proof

--- Exercici10/src/hanoi/HanoiImpl.java
+++ Exercici10/src/hanoi/HanoiImpl.java
@@ -6,12 +6,17 @@ public class HanoiImpl<E extends Comparable<E>> implements Hanoi<E> {
 
     @Override
     public void recursiuHanoi(int h, Stack<E> a, Stack<E> b, Stack<E> c) {
+        //h==1 és el cas base, moure l'element d'A a C
         if (h == 1) {
             moverDisco(a, c);
         } else {
+            //el conjunt de dades es redueix linealment amb "h"
+            //La precondició es compleix, sent sempre tres piles ordenades de major a menor
             recursiuHanoi(h - 1, a, c, b);
             moverDisco(a, c);
             recursiuHanoi(h - 1, b, a, c);
+            //Sempre es retorna les tres piles amb els elements ordenats de major a menor
+            //Avançant cap a la solució final
         }
     }
 
