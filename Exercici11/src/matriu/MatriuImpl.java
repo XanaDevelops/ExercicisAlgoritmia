1568b1d746b3858f3097568fcf370b3bf8d2b4cf
Daniel GV <danielgv.space@gmail.com>
2023-11-27 20:13:39 +0100

    Sym Recursivo, rehaciendo

--- Exercici11/src/matriu/MatriuImpl.java
+++ Exercici11/src/matriu/MatriuImpl.java
@@ -40,66 +40,64 @@ public class MatriuImpl<E extends Comparable<E>> implements Matriu<E> {
         return this.matriu[row * this.cols + col];
     }
 
-    
     @Override
     public boolean isSymmetricalRecursiu() {
         //compruebas cuadrado
         //lo que sea
         //aqui puedes comprobar el caso base
         //return isSymmetricalRecursiu(0);
-        if(this.cols!=this.rows){return false;}
-        
-        else{return this.isSymmetricalRecursiu(0);}
-        
-        
-    }
-    private boolean isSymmetricalRecursiu(int diago){
-        boolean iguals;
-     if(diago>=this.cols){return true;}
-      int i = 0;
-      while ((diago * this.cols + diago + i) < (diago * this.cols + this.rows) && (this.get(diago, diago + i).compareTo(this.get(diago + i, diago)) == 0)) {
-                i++;
-            }
-            if ((diago * this.cols + diago + i) == (diago * this.cols + this.rows)) {
-
-                iguals = isSymmetricalRecursiu(diago + 1);
-
-            } else {
-                iguals=false;
-            }
-        
-            
-        return iguals;
+        if (this.cols != this.rows) {
+            return false;
+        } else {
+            return this.isSymmetricalRecursiu(0);
         }
-        
-        
-        
-    
+
+    }
+
+    private boolean isSymmetricalRecursiu(int diago) {
+        boolean iguals;
+        if (diago >= this.cols) {
+            return true;
+        }
+        int i = 0;
+        while ((diago * this.cols + diago + i) < (diago * this.cols + this.rows) && (this.get(diago, diago + i).compareTo(this.get(diago + i, diago)) == 0)) {
+            i++;
+        }
+        if ((diago * this.cols + diago + i) == (diago * this.cols + this.rows)) {
+
+            iguals = isSymmetricalRecursiu(diago + 1);
+
+        } else {
+            iguals = false;
+        }
+
+        return iguals;
+    }
 
     /**
-     * {@inheritDoc}
-     * Cas inicial: Matriu 1x1. Retorna True <br>
+     * {@inheritDoc} Cas inicial: Matriu 1x1. Retorna True <br>
      * Cas general: Matriu NxN.
-     * @return 
+     *
+     * @return
      */
     @Override
     public boolean isSymmetricalIteratiu() {
         //si quadrada
-        if (this.cols != this.rows){
+        if (this.cols != this.rows) {
             return false;
         }
         //si cas base
-        if( this.cols*this.rows==1){
+        if (this.cols * this.rows == 1) {
             return true;
         }
-        
+
         //iterar
-        int h=0;
-        while(h<this.cols){
-            System.out.println("h: "+h);
-            for(int i = h; h+i<this.cols;i++){
+        int h = 0;
+        while (h < this.cols) {
+            System.out.println("h: " + h);
+            for (int i = h; h + i < this.cols; i++) {
                 //System.out.println("DBG: "+get(h,h+i)+" "+get(h+i,h));
-                if(get(h,h+i).compareTo(get(h+i,h))!=0){
+                if (get(h, h + i).compareTo(get(h + i, h)) != 0) {
                     return false;
                 }
             }
