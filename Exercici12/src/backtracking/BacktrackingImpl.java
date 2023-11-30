dc84ff33171d3b6a105cc014fd5a6f1546ca413c
XanaDevelops <danielgv.space@gmail.com>
2023-11-30 12:32:54 +0100

    Ex12 esqueleto

---
+++ Exercici12/src/backtracking/BacktrackingImpl.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package backtracking;
+
+import java.util.ArrayList;
+
+/**
+ * Classe implementació Backtracking
+ *
+ * @author antoni
+ */
+public class BacktrackingImpl implements Backtracking {
+
+    @Override
+    public ArrayList<ArrayList<Integer>> sumM(int[] a, int M) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+}
