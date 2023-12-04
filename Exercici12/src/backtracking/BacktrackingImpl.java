96c258cb9a6fc8f282e72564eeb10b793eccb5fd
Daniel GV <danielgv.space@gmail.com>
2023-12-04 23:42:49 +0100

    Ex12 funciona? hacer tests

--- Exercici12/src/backtracking/BacktrackingImpl.java
+++ Exercici12/src/backtracking/BacktrackingImpl.java
@@ -15,49 +15,62 @@ import java.util.Arrays;
  */
 public class BacktrackingImpl implements Backtracking {
 
-    
     @Override
     public ArrayList<ArrayList<Integer>> sumM(int[] a, int M) {
         int[] t = new int[a.length];
-        return sumM(a, M, t, 0);
+        ArrayList<ArrayList<Integer>> sols = new ArrayList<>();
+        return sumM(a, M, t, 0, sols);
     }
 
-    private ArrayList<ArrayList<Integer>> sumM(int[] a, int M, int[] t, int k) {
+    private ArrayList<ArrayList<Integer>> sumM(int[] a, int M, int[] t, int k,
+            ArrayList<ArrayList<Integer>> sols) {
         t[k] = -1;
         while (t[k] < 1) {
             t[k]++;
-            if ((sum(a, t, k) == M) && (k == a.length - 1)) {
-                printSol(a, t);
-            } else if ((sum(a, t, k) <= M) && (k < a.length - 1)) {
-                sumM(a, M, t, k + 1);
+            int s = sum(a,t,k);
+            if ((s == M) && (k == a.length - 1)) {
+                printSol(a, t, sols);
+            } else if ((s <= M) && (k < a.length - 1)) {
+                sumM(a, M, t, k + 1, sols);
 
             }
 
         }
+
         t[k] = -1;
-        return null;
+        return sols;
     }
 
-    private int sum(int[] a, int[] t, int k){
+    private int sum(int[] a, int[] t, int k) {
         System.out.println("SUM lol: ");
         System.out.println(Arrays.toString(a));
         System.out.println(Arrays.toString(t));
         System.out.println(k);
-        int r=0;
-        for(int i=0;i<a.length;i++){
-            if(t[i]>0){
-                r+=a[i];
+        int r = 0;
+        for (int i = 0; i < a.length; i++) {
+            if (t[i] > 0) {
+                r += a[i];
             }
         }
-        System.out.println("SUM RES: "+r);
+        System.out.println("SUM RES: " + r);
         return r;
     }
-    
-    private void printSol(int[] a, int[] t){
-        System.out.println("SOL: ");
+
+    private void printSol(int[] a, int[] t, ArrayList<ArrayList<Integer>> sols) {
+        System.out.println("------ SOL ------ ");
         System.out.println(Arrays.toString(a));
         System.out.println(Arrays.toString(t));
+        System.out.println("------ SOL ------ ");
+        ArrayList<Integer> r = new ArrayList<>();
+        for (int i = 0; i < a.length; i++) {
+            if (t[i] > 0) {
+                r.add(a[i]);
+            }
+        }
+        sols.add(r);
+
     }
+
     private void printSol(ArrayList<Integer> a) {
         System.out.println(a);
     }
