f255583210f937a55759be1265d983a6d2978434
Daniel GV <danielgv.space@gmail.com>
2023-12-08 19:36:56 +0100

    Ex13

---
+++ Exercici13/src/heap/HeapSortImpl.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package heap;
+
+import java.util.Random;
+
+/**
+ *
+ * @author antoni
+ */
+public class HeapSortImpl<E extends Comparable<E>> implements HeapSort<E> {
+
+    @Override
+    public void heapSort(E[] h) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+}
