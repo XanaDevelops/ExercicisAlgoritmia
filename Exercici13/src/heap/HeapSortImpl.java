317d98aecc4d3deaa9d446804da3d7542d233606
Daniel GV <danielgv.space@gmail.com>
2023-12-12 17:13:56 +0100

    Comentario pregunta AD Ex13

--- Exercici13/src/heap/HeapSortImpl.java
+++ Exercici13/src/heap/HeapSortImpl.java
@@ -5,10 +5,14 @@
  */
 package heap;
 
-import java.util.Random;
-
 /**
- *
+ * Per el teorema del quocient, tenim 2 cridades recursives, un element reductor
+ * de valor 2 i una alfa igual a 1, donant un valor de O(nlog<sub>2</sub>(n)
+ * En l'ús de CPU és costós ja que realitza moltes comparacions i desplaçaments.
+ * En l'ús de memòria es bastant eficient ja que s'ordena sobre el mateix array.
+ * Es podria optimitzar més encara si la implementació fos iterativa, ja que no
+ * s'emplearia el stack de cridades.
+ * 
  * @author Pere, Daniel
  * @author antoni
  * @param <E> Tipus dades a ordenar
@@ -17,6 +21,7 @@ public class HeapSortImpl<E extends Comparable<E>> implements HeapSort<E> {
 
     /**
      * {@inheritDoc }
+     *
      * @pre ∀i: 0&lt;=i&lt;h.length: h<sub>i</sub>!=null
      * @post ∀i: 0&lt;=i&lt;h.length-1: h<sub>i</sub>&lt;h<sub>i+1</sub>
      * @ord O(nlog<sub>2</sub>(n))
@@ -24,28 +29,25 @@ public class HeapSortImpl<E extends Comparable<E>> implements HeapSort<E> {
      */
     @Override
     public void heapSort(E[] h) {
-     int n = h.length;
+        int n = h.length;
 
         //construim el maxim monticle
         for (int i = n / 2 - 1; i >= 0; i--) {
             heapify(h, n, i);
         }
 
-        
+        //colocam en ordre
         for (int i = n - 1; i > 0; i--) {
-           
             E temp = h[0];
             h[0] = h[i];
             h[i] = temp;
 
             heapify(h, i, 0);
-        }   
+        }
     }
-    
-    
-   
+
     private void heapify(E[] h, int n, int i) {
-        int mayor = i; 
+        int mayor = i;
         int izquierda = 2 * i + 1; //fill esquerra 
         int derecha = 2 * i + 2; // fill dret 
 
@@ -69,15 +71,6 @@ public class HeapSortImpl<E extends Comparable<E>> implements HeapSort<E> {
             // Miram el subarbre
             heapify(h, n, mayor);
         }
-    }  
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
+    }
+
 }
