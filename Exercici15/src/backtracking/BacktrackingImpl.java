0d71bfd785df00cdb11cd2425e1f4d4a1e18d2d7
XanaDevelops <danielgv.space@gmail.com>
2023-12-14 12:15:56 +0100

    Ex15

---
+++ Exercici15/src/backtracking/BacktrackingImpl.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package backtracking;
+
+/**
+ *
+ * @author antoni
+ */
+public class BacktrackingImpl implements Backtracking {
+
+    @Override
+    public boolean mapColor(int[][] map, int nColors) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+}
---
+++ Exercici15/test/backtracking/BacktrackingImplTest.java
@@ -0,0 +1,33 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/UnitTests/JUnit4TestClass.java to edit this template
+ */
+package backtracking;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author antoni
+ */
+public class BacktrackingImplTest {
+    
+    public BacktrackingImplTest() {
+    }
+
+    /**
+     * Test of mapColor method, of class BacktrackingImpl.
+     */
+    @Test
+    public void testMapColor() {
+        System.out.println("mapColor");
+        int[][] map = {{1,3},{0,2,3,4},{1,4},{0,1},{1,2}};
+        int nColors = 3;
+        BacktrackingImpl instance = new BacktrackingImpl();
+        boolean expResult = true;
+        boolean result = instance.mapColor(map, nColors);
+        assertEquals(expResult, result);
+    }
+    
+}
