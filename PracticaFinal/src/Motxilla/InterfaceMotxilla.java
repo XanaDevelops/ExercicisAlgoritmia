12bbea9d8b5ef842a71e480e27348af7faa54a2e
Pere55 <truficonosmanut@gmail.com>
2023-12-30 11:01:24 +0100

    Añadido algoritmo de la mochila iterativo y recursivo

---
+++ PracticaFinal/src/Motxilla/InterfaceMotxilla.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Motxilla;
+
+/**
+ *
+ * @author trufi
+ */
+public interface InterfaceMotxilla {
+    
+       public int[] knapSack(int W, int w[], int p[]);
+       public int[] knapSackIter(int W, int w[], int p[]);
+}
