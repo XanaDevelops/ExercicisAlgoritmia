8011ba58aa150df477537d94cf196e3cc59ec449
Pere55 <truficonosmanut@gmail.com>
2023-12-30 17:17:51 +0100

    petit arreglo

---
+++ PracticaFinal/src/Motxilla/Motxilla2Pesos.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Motxilla;
+
+/**
+ *
+ * @author trufi
+ */
+public class Motxilla2Pesos {
+    
+}
