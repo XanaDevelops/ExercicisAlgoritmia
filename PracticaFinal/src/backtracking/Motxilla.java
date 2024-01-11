fd3a4b2a3198a2dcfe9283c81651fdfe6a57a23e
Pere55 <truficonosmanut@gmail.com>
2024-01-11 12:47:12 +0100

    ordre de complexitat, cas base i general

--- PracticaFinal/src/backtracking/Motxilla.java
+++ PracticaFinal/src/backtracking/Motxilla.java
@@ -24,7 +24,23 @@ public interface Motxilla {
     public ElementMotxilla[] recursiu(ElementMotxilla  a[], double W1, double W2);
     
     /**
-     * Resol la variant de la motxilla de l'enunciat de forma iterativa
+     * Resol la variant de la motxilla de l'enunciat de forma iterativa<br>
+     * 
+     * 
+     * *<br> <br>Cas base:  El cas base és dona quan ja no queden possibles combinacions que no superin algun dels pesos de la 
+     * motxilla en aquest cas s'acaba la recursio es retorna la solució possibles amb mayor profit.<br><br>
+     * Cas General:  En el cas general miram si el element actual se ha d'incluir o no ,aixo es mira comprovant si al incluir 
+     * el objecte algun dels dos pesos supera la capacitat maxima de la motxilla per el pes en especific.Tambe miram si el profit de la 
+     * combinació actual es millor a l'anterior si es aixi copiam la solució temporal sobre l'array que usarem per retornar de solució.En el cas de que  algun dels pesos actuals
+     * ya sigui igual al seu pes maxim aixo vol dir que ya no es poden incloure mes per tant retrocedirem en el array de elements <br>
+     * 
+     * @ord El algoritme es de complexitat  O(2**n) aixo es deu a que per cada element del array tenim dos possibles camins 
+     * ,agafar o no agafar el element per lo tant en el pitjor dels casos tendrem 2**n combinacions.Si ho miram per 
+     * el teorema ade la substració arribarem a la mateixa conclusió ja que a cada iteració en el pitjor dels casos feriem dos cridades 
+     * recursives una amb l'element a 0 i una amb l'element a 1 per lo tant a=2 per lo tant ens troberem en el cas de a>1,despres podem 
+     * veure que c=1 ya que anam avançant de un en un sobre el array de elements finalment com podem veure n es igual al nombre de elements a provar.
+     * Per lo tant si substituim per la formula corresponent que en aquest cas es O(a**n/c) obtenim com ha resultat O(2**n).
+     * on n es el numero de elements possibles.
      * @param a conjunt d'elements
      * @param W1 >= 0, límit per al weight1
      * @param W2 >= 0, límit per al weight2
